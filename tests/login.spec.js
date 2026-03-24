bfbc515cf8dc431655ec63dc922a21c4d8c8e54b
Andrey Skalubo <andreyskalubo@gmail.com>
2026-03-25 00:23:26 +0300

    Add test.describe

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,16 +1,21 @@
 import { test, expect } from '@playwright/test'
 
-test('check login credentials', async ({ page }) => {
-  await page.goto('https://www.saucedemo.com/')
-  await page.getByRole('textbox', { name: 'Username' }).fill('standard_user')
-  await page.getByRole('textbox', { name: 'Password' }).fill('secret_sauce')
-  await page.getByRole('button', { name: 'Login' }).click()
-  await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html')
-})
-test('check bad login credentials', async ({ page }) => {
-  await page.goto('https://www.saucedemo.com/')
-  await page.getByRole('textbox', { name: 'Username' }).fill('locked_out_user')
-  await page.getByRole('textbox', { name: 'Password' }).fill('secret_sauce')
-  await page.getByRole('button', { name: 'Login' }).click()
-  await expect(page.getByRole('heading', { name: 'Epic sadface: Sorry, this user has been locked out.' })).toBeVisible()
+test.describe('Sauce Demo authorisation', () => {
+
+  test('check login credentials', async ({ page }) => {
+    await page.goto('https://www.saucedemo.com/')
+    await page.getByRole('textbox', { name: 'Username' }).fill('standard_user')
+    await page.getByRole('textbox', { name: 'Password' }).fill('secret_sauce')
+    await page.getByRole('button', { name: 'Login' }).click()
+    await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html')
+  })
+
+  test('check bad login credentials', async ({ page }) => {
+    await page.goto('https://www.saucedemo.com/')
+    await page.getByRole('textbox', { name: 'Username' }).fill('locked_out_user')
+    await page.getByRole('textbox', { name: 'Password' }).fill('secret_sauce')
+    await page.getByRole('button', { name: 'Login' }).click()
+    await expect(page.getByRole('heading', { name: 'Epic sadface: Sorry, this user has been locked out.' })).toBeVisible()
+  })
+  
 })
